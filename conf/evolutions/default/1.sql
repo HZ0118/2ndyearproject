1bfec7993c3332ce8f0d04699fe2b15bb1122232
Enda Lee <enda.lee@ittdublin.ie>
2017-03-29 10:54:49 +0100

    added banner

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -14,19 +14,9 @@ create table flight (
 );
 create sequence flight_seq;
 
-create table user (
-  email                         varchar(255) not null,
-  role                          varchar(255),
-  name                          varchar(255),
-  password                      varchar(255),
-  constraint pk_user primary key (email)
-);
-
 
 # --- !Downs
 
 drop table if exists flight;
 drop sequence if exists flight_seq;
-
-drop table if exists user;
 
