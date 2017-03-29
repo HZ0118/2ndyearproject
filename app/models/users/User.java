10cf8f1ae8a54ed88eda5dc50f3b84ec6b62608c
Enda Lee <enda.lee@ittdublin.ie>
2017-03-29 17:37:25 +0100

    login link needs to be added

--- app/models/users/User.java
+++ app/models/users/User.java
@@ -23,10 +23,48 @@ public class User extends Model {
 
 
   public User(){
-}
+  }
 
   public static Finder<String,User> find = new Finder<String,User>(User.class);
 
   public static List<User> findAll(){
 	return User.find.all();
+  }
+
+  public static User authenticate(String email,String password){
+	return find.where().eq("email",email).eq("password",password).findUnique();
+  }
+
+  public String getEmail(){
+	return email;
+  }
+
+  public void setEmail(String email){
+	this.email = email;
+  }
+
+  public String getRole(){
+	return role;
+  }
+
+  public void setRole(String role){
+	this.role = role;
+  }
+
+  public String getName(){
+	return name;
+  }
+ 
+  public void setName(String name){
+ 	this.name = name;
+  }
+
+  public String getPassword(){
+	return password;
+  }
+
+  public void setPassword(String password){
+	this.password = password;
+  }
 }
+
